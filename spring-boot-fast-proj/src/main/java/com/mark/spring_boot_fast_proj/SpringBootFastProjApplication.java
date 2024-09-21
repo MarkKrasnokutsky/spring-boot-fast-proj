be9bec7cb9f6c3c0344a88889df3939bf744a199
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-09-21 17:26:14 +0500

    Init proj

---
+++ spring-boot-fast-proj/src/main/java/com/mark/spring_boot_fast_proj/SpringBootFastProjApplication.java
@@ -0,0 +1,14 @@
+package com.mark.spring_boot_fast_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFastProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFastProjApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ spring-boot-fast-proj/src/test/java/com/mark/spring_boot_fast_proj/SpringBootFastProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.spring_boot_fast_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFastProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
