ef17faeb9ca9e14e0fa6cd34adafb2c6c398a93f
Mark Krasnokuysky <Mark120320041234@mail.ru>
2024-09-21 17:33:25 +0500

    Added class Coffee.

--- spring-boot-fast-proj/src/main/java/com/mark/spring_boot_fast_proj/SpringBootFastProjApplication.java
+++ spring-boot-fast-proj/src/main/java/com/mark/spring_boot_fast_proj/SpringBootFastProjApplication.java
@@ -3,6 +3,8 @@ package com.mark.spring_boot_fast_proj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class SpringBootFastProjApplication {
 
@@ -10,5 +12,30 @@ public class SpringBootFastProjApplication {
 		SpringApplication.run(SpringBootFastProjApplication.class, args);
 		System.out.println("Hello World!");
 	}
+}
 
+class Coffee {
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+	public void SetName(String name) {
+		this.name = name;
+	}
 }
